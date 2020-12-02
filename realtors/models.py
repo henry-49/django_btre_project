e57073d286dc1548a3e3ac7c43373202eea04064
henry-49 <henryswebdevelopment@gmail.com>
2020-12-02 18:50:09 +0000

    Forth commit

--- realtors/models.py
+++ realtors/models.py
@@ -8,7 +8,7 @@ class Realtor(models.Model):
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email =  models.CharField(max_length=50)
-    is_mvp: models.BooleanField(default=False)
+    is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.name
