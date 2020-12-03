5ad6ca7195187124747ad265d22495c7a0e2a8f8
henry-49 <henryswebdevelopment@gmail.com>
2020-12-03 11:33:28 +0000

    add humanize in the btre setting file and syntax to display dynamic page for listings dot html

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
+
+
 
 # Create your views here.
-
 def index(request):
-    return render(request, 'listings/listings.html')
+    # Fetching data from the database
+    listings = Listing.objects.all()
 
-def listing(request):
+    # Creating dictonary
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
