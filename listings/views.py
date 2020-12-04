620d65a528d718d0f87f7066581f9e771a0b9e96
henry-49 <henryswebdevelopment@gmail.com>
2020-12-04 10:24:49 +0000

    modified listings html file in the template folder

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Listing
@@ -7,11 +8,18 @@ from .models import Listing
 # Create your views here.
 def index(request):
     # Fetching data from the database
-    listings = Listing.objects.all()
+    # listings = Listing.objects.all()
+    # oder by -list_date desc and filter is_published=True shows when is true
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    #Using Paginator displays limit of 6 pages
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
 
     # Creating dictonary
     context = {
-        'listings' : listings
+        'listings' : page_listings
     }
     return render(request, 'listings/listings.html', context)
 
