0072d89254e6f12ce6625ab44aef5acb8c21880f
henry-49 <henryswebdevelopment@gmail.com>
2020-12-04 13:03:06 +0000

    modified listings, about, index html file in the templates slash page folder

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+     # Fetching data from the database
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+     # Creating dictonary
+    context = {
+         'listings' : listings
+     } 
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP 
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    # Creating dictonary
+    context = {
+        'realtors' : realtors,
+        'mvp_realtors' : mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
