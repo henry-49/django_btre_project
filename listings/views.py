88c86b94707d67a72c238f82750f0b0423a264ee
henry-49 <henryswebdevelopment@gmail.com>
2020-12-05 14:06:07 +0000

    added choices dictionary to listings root folder

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from .choices import price_choices, bedroom_choices, state_choices
 from django.http import HttpResponse
 from .models import Listing
 from .models import Realtor
@@ -37,4 +38,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    # Creating dictonary
+    context = {
+         'state_choices' : state_choices,
+         'bedroom_choices' : bedroom_choices,
+         'price_choices' : price_choices
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
+
 
 # Create your views here.
-
 def index(request):
      # Fetching data from the database
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
      # Creating dictonary
     context = {
-         'listings' : listings
+         'listings' : listings,
+         'state_choices' : state_choices,
+         'bedroom_choices' : bedroom_choices,
+         'price_choices' : price_choices
+
      } 
 
     return render(request, 'pages/index.html', context)
