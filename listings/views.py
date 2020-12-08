99667943fbcfe87a313951d9ab5a26b38b7d217d
henry-49 <henryswebdevelopment@gmail.com>
2020-12-08 09:28:47 +0000

    added search filtering in the search file

--- listings/views.py
+++ listings/views.py
@@ -38,10 +38,48 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    # query for search
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    # iexact means case insensitive 'i' | case sensitive will just be written as 'exact'
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    # iexact means case insensitive 'i' | case sensitive will just be written as 'exact'
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    # lte less than or equal to | <=
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    
+    # Price
+    # lte less than or equal to | <=
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
     # Creating dictonary
     context = {
          'state_choices' : state_choices,
          'bedroom_choices' : bedroom_choices,
-         'price_choices' : price_choices
+         'price_choices' : price_choices,
+         'listings' : queryset_list
     }
     return render(request, 'listings/search.html', context)
