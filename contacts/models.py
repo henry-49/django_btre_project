4fdb7cb3aa03879dc0cceb1d2b4c6487d0c7327e
henry-49 <henryswebdevelopment@gmail.com>
2020-12-09 11:49:48 +0000

    Added ContactAdmin to display on the backend and remigrations of contact model

--- contacts/models.py
+++ contacts/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     message = models.TextField(blank=True)
-    concact_date = models.DateTimeField(default=datetime.now, blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
     def __str__(self):
         return self.name
